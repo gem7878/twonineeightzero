46321b6fe5304a767b136e34023cf0a4dc61950f
pieceofjc <pieceofjc@gmail.com>
2023-06-01 22:44:25 +0900

    first commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("안녕하세요!");
+  res.send("안녕하세요!!!!!!!!!!!");
 });
 app.listen(3000, () => console.log("3000포트에서 대기 중..."));
