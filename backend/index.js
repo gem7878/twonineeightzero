ec71db02f20d372d1f54ef1284ea559b1bc4e8e4
pieceofjc <pieceofjc@gmail.com>
2023-06-07 21:35:54 +0900

    전역, 후역 검색

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const xlsxRouter = require('./routes/station');
 
 app.get("/", function (req, res) {
   res.send("안녕하세요!!!!!!!!!!!");
 });
+
+app.use('/db',xlsxRouter);
+
 app.listen(3000, () => console.log("3000포트에서 대기 중..."));
