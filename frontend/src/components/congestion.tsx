c6946feca8f04098f834a9830672688e2d65368c
dev-minjeong <vitaminjeong1026@gmail.com>
2023-06-10 23:08:37 +0900

    수정

--- frontend/src/components/congestion.tsx
+++ frontend/src/components/congestion.tsx
@@ -2,23 +2,19 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styled from 'styled-components/native';
 
-interface CongestionProps {
-}
-
-const Congestion: React.FC<CongestionProps> = props => {
-
+const Congestion: React.FC = () => {
   return (
     <View>
-        <CongestionBox>
-            <Text>__역의 퍙군적인 혼잡도는 __%입니다.</Text>
-        </CongestionBox>
+      <CongestionBox>
+        <Text>__역의 퍙군적인 혼잡도는 __%입니다.</Text>
+      </CongestionBox>
     </View>
   );
 };
 
 const CongestionBox = styled.View`
-  width: 284;
-  height: 100;
+  width: 284px;
+  height: 100px;
   border: 1px solid black;
 `;
 
