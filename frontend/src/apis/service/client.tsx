ed0cb9a5049aca1f1e9371c63e0de921d6003eda
pieceofjc <pieceofjc@gmail.com>
2023-07-25 23:19:57 +0900

    호선별 api axios 부분 수정

--- frontend/src/apis/service/client.tsx
+++ frontend/src/apis/service/client.tsx
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const axiosInstance = axios.create({baseURL: 'http://localhost:3000'});
+const axiosInstance = axios.create({baseURL: 'http://10.0.2.2:3000'});
 export default axiosInstance
