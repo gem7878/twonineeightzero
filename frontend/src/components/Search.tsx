95eae2f8dd043c69d44d64ef7c82baaa7b902346
dev-minjeong <vitaminjeong1026@gmail.com>
2023-09-09 19:00:44 +0900

    프론트 수정

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 
--- frontend/src/components/menu/SignIn.tsx
+++ frontend/src/components/menu/SignIn.tsx
@@ -1,26 +1,27 @@
-import React, { useRef } from 'react';
-import { Text, Button, Alert, View } from "react-native";
-import { useForm, Controller } from "react-hook-form";
+import React from 'react';
+import {Text, Button, Alert, View} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
 import styled from 'styled-components/native';
 import axiosInstance from '../../apis/service/client';
 
-const SignIns: React.FC = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm({
+const SignIn: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
     defaultValues: {
       ID: '',
       Password: '',
-    }
+    },
   });
-  
-  const onSubmit = async (data : any) => {
+
+  const onSubmit = async (data: any) => {
     await axiosInstance
-      .post(
-        '/user/login',
-        {
-          user_name: data.ID,
-          user_password: data.Password
-        }
-      )
+      .post('/user/login', {
+        user_name: data.ID,
+        user_password: data.Password,
+      })
       .then(function (res: any) {
         if (res.status === 200) {
           console.log(res.data.message);
@@ -29,30 +30,28 @@ const SignIns: React.FC = () => {
       .catch(function (error: any) {
         Alert.alert('Error', error.response.data.error);
       });
-  }
-
+  };
 
   return (
     <ContentContainer>
-        <LeftText>아이디</LeftText>
+      <LeftText>아이디</LeftText>
       <Controller
         control={control}
         rules={{
-            minLength: {
-                value: 8,
-                message: "아이디 8글자 이상 입력하시오"
-            },
-            maxLength: {
-                value: 15,
-                message: "아이디를 15글자 이하 입력하시오"
-            },
-            required: {
-                value: true,
-                message: "아이디를 입력하시오"
-            }
-
+          minLength: {
+            value: 8,
+            message: '아이디 8글자 이상 입력하시오',
+          },
+          maxLength: {
+            value: 15,
+            message: '아이디를 15글자 이하 입력하시오',
+          },
+          required: {
+            value: true,
+            message: '아이디를 입력하시오',
+          },
         }}
-        render={({ field: { onChange, onBlur, value } }) => (
+        render={({field: {onChange, onBlur, value}}) => (
           <FormInput
             placeholder="아이디를 입력하시오"
             onBlur={onBlur}
@@ -65,24 +64,24 @@ const SignIns: React.FC = () => {
       {errors.ID && <Text style={{color: 'red'}}>{errors.ID.message}</Text>}
       {errors.ID && <Text style={{color: 'red'}}>{errors.ID.message}</Text>}
 
-        <LeftText>비밀번호</LeftText>
+      <LeftText>비밀번호</LeftText>
       <Controller
         control={control}
         rules={{
-            minLength: {
-                value: 8,
-                message: "비밀번호를 8글자 이상 입력하시오"
-            },
-            maxLength: {
-                value: 20,
-                message: "비밀번호를 20글자 이하 입력하시오"
-            },
-            required: {
-                value: true,
-                message: "비밀번호를 입력하시오"
-            }
+          minLength: {
+            value: 8,
+            message: '비밀번호를 8글자 이상 입력하시오',
+          },
+          maxLength: {
+            value: 20,
+            message: '비밀번호를 20글자 이하 입력하시오',
+          },
+          required: {
+            value: true,
+            message: '비밀번호를 입력하시오',
+          },
         }}
-        render={({ field: { onChange, onBlur, value } }) => (
+        render={({field: {onChange, onBlur, value}}) => (
           <FormInput
             secureTextEntry={true}
             placeholder="비밀번호를 입력하시오"
@@ -93,39 +92,44 @@ const SignIns: React.FC = () => {
         )}
         name="Password"
       />
-        {errors.Password && <Text style={{color: 'red'}}>{errors.Password.message}</Text>}
-        
+      {errors.Password && (
+        <Text style={{color: 'red'}}>{errors.Password.message}</Text>
+      )}
 
-      <View style={{margin:20}}>
-        <Button color={'#00ffd1'} title="로그인" onPress={handleSubmit(onSubmit)}/>
+      <View style={{margin: 20}}>
+        <Button
+          color={'#00ffd1'}
+          title="로그인"
+          onPress={handleSubmit(onSubmit)}
+        />
       </View>
     </ContentContainer>
   );
-}
+};
 
 const ContentContainer = styled.View`
-    width: 70%;
-    margin: 0 auto;
-    margin-top: -30px;
-    justify-content: center;
-    flex: 1;
+  width: 70%;
+  margin: 0 auto;
+  margin-top: -30px;
+  justify-content: center;
+  flex: 1;
 `;
 
 const LeftText = styled.Text`
-    text-align: left;
-    margin-top: 20px;
-    color: black;
-    font-size: 14px;
-    font-weight: 200;
+  text-align: left;
+  margin-top: 20px;
+  color: black;
+  font-size: 14px;
+  font-weight: 200;
 `;
 
 const FormInput = styled.TextInput`
-    border-radius: 4px;
-    border: 1px solid black;
-    padding: 10px 15px;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    font-size: 14px;
+  border-radius: 4px;
+  border: 1px solid black;
+  padding: 10px 15px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-size: 14px;
 `;
 
-export default SignIns;
+export default SignIn;
