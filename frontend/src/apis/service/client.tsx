8a8a02e25f6ba14370c3fa0892a3dc19b57926ce
pieceofjc <pieceofjc@gmail.com>
2023-09-20 18:57:29 +0900

    url 수정

--- frontend/src/apis/service/client.tsx
+++ frontend/src/apis/service/client.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://secure-unison-398308.du.r.appspot.com',
-}); // http://10.0.2.2:3000
+}); // http://10.0.2.2:4000
 
 export default axiosInstance;
