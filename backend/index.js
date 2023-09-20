8deecb4f4297561f2ee80c1ff67ea36754b3095c
pieceofjc <pieceofjc@gmail.com>
2023-09-20 18:54:58 +0900

    모듈 타입으로 수정

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const app = express();
-const apiRouter = require("./routes/station");
-const congRouter = require("./routes/congestion");
+import apiRouter from './routes/station.routes.js';
+import congRouter from './routes/congestion.routes.js';
+import userRouter from './routes/user.routes.js';
 
 let corsOptions = {
   origin: "*",
   Credential: true,
 };
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
-  res.send("안녕하세요!!!!!!!!!!!");
+  res.send("이구팔땡 API 서버입니다");
 });
 
-app.use("/api", apiRouter);
-app.use("/cong", congRouter);
 
-app.listen(process.env.PORT || 3000);
-// app.listen(process.env.PORT || 4000);
+app.use('/api',apiRouter);
+app.use('/cong',congRouter);
+app.use('/user',userRouter);
+
+app.listen(process.env.PORT || 4000);
