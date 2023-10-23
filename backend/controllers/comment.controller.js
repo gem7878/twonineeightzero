38ca1ea6da803ee28be482c44659520ba045b41d
pieceofjc <pieceofjc@gmail.com>
2023-10-23 14:07:42 +0900

    comment findall 수정

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -3,27 +3,27 @@ const Comment = db.comment;
 
 export async function findAll(req, res) {
     const postId = req.params.id;
-    const pageNumber = req.params.num;
-    const limit = 5;
+    // const pageNumber = req.params.num;
+    // const limit = 5;
     
     try {
-        const data = await Comment.findAndCountAll({
+        const data = await Comment.findAll({
             where: {customerBoardId:postId},
             attributes: ['userAccountUserId', 'content', 'createdAt'],
             order: [['createdAt', 'ASC']],
-            offset: (pageNumber-1)*limit,
-            limit: limit,
+            // offset: (pageNumber-1)*limit,
+            // limit: limit,
         })
     
 
-        const countPage = Math.ceil(data.count / limit);
-        if (pageNumber > countPage) res.status(400).send({message: "잘못된 페이지입니다."});
+        /* const countPage = Math.ceil(data.count / limit);
+        if (pageNumber > countPage) res.status(400).send({message: "잘못된 페이지입니다."});*/
         
-        const comments = await Promise.all(data.rows.map(async (comment) => { return {comment, editable : comment.userAccountUserId === req.user_id ? true : false}}));
+        const comments = await Promise.all(data.map(async (comment) => { 
+            return {comment, editable : comment.userAccountUserId === req.editable ? true : false}
+        }));
         
         await res.status(200).send({
-            "countAllComment": data.count,
-            "countPage": Math.ceil(data.count / limit),
             "data": comments,
         });
 
@@ -50,7 +50,7 @@ export function writeComment(req, res) {
     })
     .catch((err) => {
         res.status(500).send({
-            message: err.message || "Write Error"
+            message: "Write Comment Error: " + err.message,
         })
     });
 }
@@ -71,11 +71,12 @@ export function updateComment(req, res) {
             res.status(400).send({
                 message: "기존의 값과 같거나 권한이 없습니다."
             });
-        } else {
-            res.status(400).send({
-                message: "some error occurred"
-            });
         }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Update Comment Error: "+err.message
+        });
     });
 }
 
@@ -95,12 +96,12 @@ export function deleteComment(req, res) {
                 success: false,
                 message: "post를 찾지 못했거나 권한이 없습니다."
             })
-        } else {
-            res.status(400).send({
-                success: false,
-                message: "some error occurred"
-            });
         }
-    });  
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Delete Comment Error: "+err.message
+        });
+    });
 
 }
