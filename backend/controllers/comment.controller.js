c6417357b6dbf049ef32294694f8bf9e301a7032
pieceofjc <pieceofjc@gmail.com>
2023-10-23 15:25:47 +0900

    comment findall api data 수정

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import db from "../models/index.js";
 const Comment = db.comment;
+const User = db.user;
 
 export async function findAll(req, res) {
     const postId = req.params.id;
@@ -9,23 +10,30 @@ export async function findAll(req, res) {
     try {
         const data = await Comment.findAll({
             where: {customerBoardId:postId},
-            attributes: ['userAccountUserId', 'content', 'createdAt'],
+            attributes: ['userAccountUserId', 'content', 'updatedAt'],
             order: [['createdAt', 'ASC']],
             // offset: (pageNumber-1)*limit,
             // limit: limit,
+            include: [
+                {
+                    model: User,
+                    attributes: ['user_name'],
+                }
+            ]
         })
-    
 
         /* const countPage = Math.ceil(data.count / limit);
         if (pageNumber > countPage) res.status(400).send({message: "잘못된 페이지입니다."});*/
         
         const comments = await Promise.all(data.map(async (comment) => { 
-            return {comment, editable : comment.userAccountUserId === req.editable ? true : false}
-        }));
+            return {
+                content: comment.content,
+                updatedAt: comment.updatedAt,
+                userName: comment.user_account.user_name,
+                editable : comment.userAccountUserId === req.editable ? true : false}
+            }));
         
-        await res.status(200).send({
-            "data": comments,
-        });
+        await res.status(200).send(comments);
 
     } catch (err) {
         res.status(500).send({
